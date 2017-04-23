e5824c0d102bce52d1b1cd8661b5a51bcf338482
vis <vis.ios@qq.com>
2017-04-23 15:23:49 +0800

    添加junit的依赖并对查询数据库user表进行单元测试

--- src/test/java/com/loveqh/test/UserTest.java
+++ src/test/java/com/loveqh/test/UserTest.java
@@ -5,23 +5,24 @@ import com.loveqh.pojo.User;
 import com.loveqh.util.MyBatisSessionFactory;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by WL on 2017-04-23.
  */
 public class UserTest {
 
-    public void findUserById(int id) {
+    @Test
+    public void findUserById() {
         SqlSessionFactory sessionFactory = MyBatisSessionFactory.getSessionFactory();
         SqlSession session = sessionFactory.openSession();
         UserDao userDao = session.getMapper(UserDao.class);
+        int id = 1;
         User user = userDao.findUserById(id);
-        System.out.println(user);
         session.close();
-    }
-
-    public static void main(String[] args) {
-        UserTest test = new UserTest();
-        test.findUserById(1);
+        System.out.println(user);
+        Assert.assertNotNull(user);
+        Assert.assertEquals(user.getId(), id);
     }
 }
