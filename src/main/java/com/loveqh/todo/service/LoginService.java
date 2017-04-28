aacc210900b8b615dde7caf4df80f559e4adc436
vis <vis.ios@qq.com>
2017-04-28 12:07:09 +0800

    修改spring mvc的配置，弄清了applicationContext.xml与xxx-servlet.xml的关系。 增加LoginService类来处理用户登录的业务逻辑，尚未实现。

---
+++ src/main/java/com/loveqh/todo/service/LoginService.java
@@ -0,0 +1,13 @@
+package com.loveqh.todo.service;
+
+import com.loveqh.todo.pojo.User;
+
+/**
+ * Created by WL on 2017-04-28.
+ */
+public class LoginService {
+
+    public boolean login(User user) {
+        return false;
+    }
+}
